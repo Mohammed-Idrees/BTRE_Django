da4b2d1d6e3d006498c183f42055f74a9db8ebea
Mohammed-Idrees <itsmohmad.edris@gmail.com>
2020-11-24 19:09:18 +0530

    Second Commit

--- btre/settings.py
+++ btre/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'btredb',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
@@ -128,3 +131,8 @@ STATICFILES_DIRS =[
     os.path.join(BASE_DIR,'btre/static')
 
 ]
+
+#Media Folder Settings
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
